872df7d1248933237acb73b8f0c6ce0c1f6eb197
vigreux-joël <vigreux.joel@hotmail.com>
2022-10-25 15:10:28 +0200

    function

--- main.py
+++ main.py
@@ -1,23 +1,22 @@
 import csv
 
-file = "user.csv"
-fieldnames = ['first_name', 'last_name', 'age', 'city']
-
-def form():
+def form(fieldnames):
     values = {}
     for fieldname in fieldnames:
         value = input("please enter the "+fieldname+" :")
         values[fieldname] = value
     return values
 
-def create():
+def create(file, fieldnames):
     with open(file, 'w', newline='') as csvfile:
         writer = csv.DictWriter(csvfile, fieldnames)
         writer.writeheader()
-        writer.writerow(form())
+        writer.writerow(form(fieldnames))
 
 def main():
-    create()
+    file = "user.csv"
+    fieldnames = ['first_name', 'last_name', 'age', 'city']
+    create(file, fieldnames)
 
 if __name__ == '__main__':
     main()
