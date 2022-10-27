7b0589ef3943cb0f051232d928a7469fdc5525a7
vigreux-joël <vigreux.joel@hotmail.com>
2022-10-27 14:26:05 +0200

    final

--- calculatrice/main.py
+++ calculatrice/main.py
@@ -1,5 +1,23 @@
+def calculatrice():
+    calcul = input('Veuillez entrer votre calcul: ')
+
+    result = exe_calcul(calcul)
+    if result:
+        print("Le résultat est:  " + str())
+    else:
+        print("Calcul invalide")
+
+
+def exe_calcul(calcul):
+    try:
+        return eval(calcul)
+    except:
+        return False
+
+
 def main():
-    pass
+    calculatrice()
+
 
 if __name__ == '__main__':
     main()
--- ip/main.py
+++ ip/main.py
@@ -1,5 +1,32 @@
+def convert_ip_to_binary(ip):
+    return ''.join([bin(int(x) + 256)[3:] for x in ip.split('.')])
+
+
+def convert_binary_to_ip(binary):
+    string = ''
+    for i in range(0, len(binary), 8):
+        value = binary[i:i + 8]
+        string += str(int(value, 2))+"."
+    return string[:-1]
+
+
+def calcul_ip():
+    ip = input("Insérer votre adresse IP: ")
+    #test: ip = "135.30.34.96/15"
+
+    ip, mask = ip.split('/')
+    base = convert_ip_to_binary(ip)[:int(mask)]
+
+    network = str(base) + ("0" * (32 - int(mask)))
+    broadcast = str(base) + ("1" * (32 - int(mask)))
+
+    print("Network: " + convert_binary_to_ip(network))
+    print("Broadcast: " + convert_binary_to_ip(broadcast))
+
+
 def main():
-    pass
+    calcul_ip()
+
 
 if __name__ == '__main__':
     main()
